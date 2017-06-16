93e7ce23c2d1047cc2ec564e29fe697f5283c920
DevYmA <ymamarathunga@gmail.com>
2017-06-16 18:29:25 +0530

    Initialize

---
+++ static/custome/js/scrolling-navgation.js
@@ -0,0 +1,41 @@
+//Smooth scrolling - note that: required jquery.easy.min.js
+$(function(){
+    $(document).on('click', 'a.page-scroll', function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+//Change active link when scrolling
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+
+  return false;
+});
